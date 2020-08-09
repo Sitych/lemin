b82a6569980de87e339ce420e174f89052661c60
qjosmyn <qjosmyn@student.21-school.ru>
2020-08-10 01:38:22 +0300

    g

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lem_in.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rretta <rretta@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qjosmyn <qjosmyn@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 19:29:35 by qjosmyn           #+#    #+#             */
-/*   Updated: 2020/08/05 06:20:40 by rretta           ###   ########.fr       */
+/*   Updated: 2020/08/09 20:59:53 by qjosmyn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,10 @@
 **	define const value for hash func
 */
 
-# define P			(int)10
-# define HTSIZE 	(int)65536
-# define INFINITY	INT32_MAX
+# define P				(int)10
+# define HTSIZE 		(int)65536
+# define INFINITY		INT32_MAX
+# define ERROR_INPUT	"ERROR: ERROR INPUT"
 
 typedef struct			s_edge
 {
@@ -75,9 +76,11 @@ typedef struct			s_all_edges
 
 typedef struct			s_graph
 {
-  int     ants; //total number of ants
+  int		ants; //total number of ants
   int 		V;	//total quantity of vertices 
   int 		E;	//total number of edges
+  t_room		*start;
+  t_room		*end;
   t_all_edges *edges;  //list of edges
 }						t_graph;
 
@@ -108,14 +111,14 @@ t_htable				*g_htable[HTSIZE];
 char			**ft_file_parse(char **split);
 t_graph			*ft_file_checker(char **split,int all, t_graph *g);
 int				ft_val_ant(char *split);
-int				ft_val_bond(char **split);
-int				ft_val_room(char **split, t_graph **g);
-int				ft_val_coords(char **split, int i);
+int		ft_val_bond(char **lines, t_graph *g);
+int				ft_val_room(char **split, t_graph **g, int i);
+int				ft_val_coords(char *x, char *y);
 char			***ft_val_links(char **split, int i, int all, t_graph **g);
 int				ft_word_counter(char const *s, char c);
 void			ft_exit(char *str);
+void			ft_check_room(char **line, char **prev_line, int i, t_graph **g);
 void			ft_prng(unsigned int seed);
-int				ft_check_room(char **split, int i);
 char			**ft_swap_links(char **str, char **link);
 int				ft_val_links2(char **split, int j, int i, char **links);
 void			ft_set_links(char ***links, int i, int all, t_graph **g);
@@ -127,6 +130,7 @@ void			ft_del_edge(t_edge *ptr);
 */
 
 t_room		*ft_crtrm(char *str, int flag);
+t_room		*ft_creat_room(char *name, int x, int y);
 t_room		*ft_add_push_back(t_room *tmp, char **split, int i);
 t_room		*ft_roomdel(t_room **ptr);
 void			ft_check_name_coord(t_room *ptr);
@@ -146,8 +150,6 @@ void			ft_create_htable(int len);
 void			ft_set_htable(char **split, int len);
 void			ft_thprint(void);
 void			ft_del_htable(void);
-t_room			*ft_find_start(void);
-t_room			*ft_find_end(void);
 
 /*
 ** FUNCS FOR QUEUE
@@ -171,14 +173,14 @@ void		ft_print_bfs(void);
 /*
 ** FUNCS FOR SET WAYS
 */
-int			ft_manage_way(t_room *room);
+int			ft_manage_way(t_graph *g, t_room *room);
 int			ft_V_counter(void);
 int			ft_E_counter(int all, int i);
 t_way_node	*ft_create_node(t_room *room_name);
 t_edge		*ft_find_min_bfs_link(t_edge *links);
 t_way		*ft_paste_first_node(t_way *way, t_way_node *node);
 t_way		*ft_paste_node(t_way *way, t_way_node *node);
-t_way		*ft_find_short_way(t_room *room, t_edge *(*func) (t_edge *link));
+t_way		*ft_find_short_way(t_graph *g, t_room *room, t_edge *(*func) (t_edge *link));
 t_way		*ft_find_all_short_ways(t_way	*ways);
 void		ft_print_way(t_way *way);
 int			ft_bellman_ford(t_graph *g, t_room *start);
@@ -190,7 +192,15 @@ t_edge		*ft_find_edge1(t_room *from, t_room *to);
 void		ft_null_ways(t_way *ways);
 t_way		*ft_find_solution(t_room *room, t_graph *g);
 t_edge		*ft_find_minus_one_link(t_edge *links);
-void	ft_null(t_graph *g);
-t_sol	*ft_solution(t_graph *g);
+void		ft_null(t_graph *g);
+// t_sol		*ft_solution(t_graph *g);
+t_way		*ft_bhandari(t_graph *g);
+t_way		*ft_solution(t_graph *g);
+t_way		*ft_del_way_return_null(t_way **way);
+int			ft_step_counter(t_way *ways, t_graph *g);
+int			ft_step_printer(t_way *ways, t_graph *g);
+t_edge		*ft_find_edge(t_room *from, t_room *to);
+void		ft_del_mas(char ***mas, int len);
+void		ft_sort_solution(t_way *way);
 
 #endif
